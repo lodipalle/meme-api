966c28e0d6182e80d56649d12aeb7b4d65bf677c
Lodi Palle <lodipalle@gmail.com>
2021-01-07 08:41:12 +1100

    added view modal

---
+++ meme-api/src/ViewModal.js
@@ -0,0 +1,16 @@
+import {Button, ListGroup, Modal} from "react-bootstrap";
+import React from "react";
+
+const image = {
+
+    width: "100%",
+    height: "280px"
+
+};
+
+const ViewModal = (props) => {
+
+
+
+}
+export default ViewModal
