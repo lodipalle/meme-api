bf8d73ffdc37b205cd0fb5e724fb0308b370138e
Lodi Palle <lodipalle@gmail.com>
2021-01-07 08:36:51 +1100

    continuing detail modal

--- meme-api/src/DetailModal.js
+++ meme-api/src/DetailModal.js
@@ -10,7 +10,29 @@ const image = {
 
 const DetailModal = (props) => {
 
-
+    const {item, showDetailModal, handleClose,handleViewShow} = props
+    return (
+        <Modal size={"lg"} show={showDetailModal} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Details</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="card">
+                    <img src={item.url} className="card-img-top" alt="..."
+                         style={image}/>
+                    <ListGroup>
+                        <ListGroup.Item>Name: {item.name}</ListGroup.Item>
+                        <ListGroup.Item><Button onClick={handleViewShow}>Find More</Button></ListGroup.Item>
+                    </ListGroup>
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
 
 }
 export default DetailModal
