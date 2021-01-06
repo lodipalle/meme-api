c4ce9b80d55c0b083d01079e4c5ae37eb16740de
Lodi Palle <lodipalle@gmail.com>
2021-01-07 08:43:04 +1100

    added view modal

--- meme-api/src/ViewModal.js
+++ meme-api/src/ViewModal.js
@@ -10,7 +10,33 @@ const image = {
 
 const ViewModal = (props) => {
 
-
+    const {item, showViewDetailModal, handleClose} = props
+    return (
+        <Modal size={"lg"} show={showViewDetailModal} onHide={handleClose}>
+            <Modal.Header closeButton>
+                <Modal.Title>Details</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <div className="card">
+                    <img src={item.url} className="card-img-top" alt="..."
+                         style={image}/>
+                    <ListGroup>
+                        <ListGroup.Item>Id: {item.id}</ListGroup.Item>
+                        <ListGroup.Item>Name: {item.name}</ListGroup.Item>
+                        <ListGroup.Item>Width: {item.width}</ListGroup.Item>
+                        <ListGroup.Item>Height: {item.height}</ListGroup.Item>
+                        <ListGroup.Item>Box Count: {item.box_count}</ListGroup.Item>
+                        <ListGroup.Item>Link: {item.url}</ListGroup.Item>
+                    </ListGroup>
+                </div>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
 
 }
 export default ViewModal
